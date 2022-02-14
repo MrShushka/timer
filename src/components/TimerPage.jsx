db1dd9bffa72dbbe0dbe3a149b49b892b0916cf6
MrShushka <mrshushka99@gmail.com>
2022-02-14 16:20:36 +0200

    newer version

---
+++ src/components/TimerPage.jsx
@@ -0,0 +1,50 @@
+import React, {useState, useEffect} from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { interval, Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+import Timer from './timer';
+import Bottons from './bottons';
+import './TimerPage.css';
+
+function TimerPage() {
+  const [time, setTime] = useState (0);
+  const [isOn, setIsON] = useState (false);
+
+  useEffect(()=>{
+    const unsubscribe = new Subject();
+    interval(1000)
+        .pipe(takeUntil(unsubscribe))
+        .subscribe(() => {
+          if (isOn) {
+            setTime(val => val + 1);
+          }
+        });
+    return () => {
+      unsubscribe.next();
+      unsubscribe.complete();
+    };
+  }, ); 
+
+  const startHandler = () =>{
+    setIsON(true);
+  }
+
+  const stopResetHandler = () => {
+    setTime(0);
+    setIsON(false);
+    console.log("rest");
+  }
+
+  const waitHandler = () => {
+    setIsON(false);
+  }
+
+  return (
+    <div className="main-section">
+      <Timer time = {time}/>
+      <Bottons wait = {waitHandler} start = {startHandler} stop = {stopResetHandler} isOn = {isOn} />
+    </div>
+  );
+}
+
+export default TimerPage;
